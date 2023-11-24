533ed3e5e107e1ad904fbc3abcdfd05cb3eb6186
Shura Shuracov <shura@shuracov.ru>
2023-11-24 16:27:03 +0300

    Improve exp_connect.py script

--- exp_connect.py
+++ exp_connect.py
@@ -1,4 +1,12 @@
+#!/usr/bin/python3
+# Import module
 import jds6600
+
+# Create object with port for internal USB<->serial converter
 j = jds6600.JDS6600(port="/dev/ttyUSB0")
+
+# Connect to device
 j.connect()
-print(j.get_channels())
+
+# Get channel info and print it
+print("Channels status:",j.get_channels())
